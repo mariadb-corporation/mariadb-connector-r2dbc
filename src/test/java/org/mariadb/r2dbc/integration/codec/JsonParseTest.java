97f9e03ea5698fbe177fcfe0cfa95484561306cf
rusher <diego.dupin@gmail.com>
2024-06-12 18:31:04 +0200

    [misc] test for JSON field addition

---
+++ src/test/java/org/mariadb/r2dbc/integration/codec/JsonParseTest.java
@@ -0,0 +1,100 @@
+// SPDX-License-Identifier: Apache-2.0
+// Copyright (c) 2020-2024 MariaDB Corporation Ab
+
+package org.mariadb.r2dbc.integration.codec;
+
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.Assumptions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+import org.mariadb.r2dbc.BaseConnectionTest;
+import org.mariadb.r2dbc.api.MariadbConnection;
+import reactor.test.StepVerifier;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public class JsonParseTest extends BaseConnectionTest {
+  @BeforeAll
+  public static void before2() {
+      afterAll2();
+      sharedConn.beginTransaction().block();
+      sharedConn.createStatement("DROP TABLE IF EXISTS JsonTable").execute().blockLast();
+      sharedConn.createStatement("CREATE TABLE JsonTable (t1 JSON)").execute().blockLast();
+      sharedConn
+          .createStatement(
+              "INSERT INTO JsonTable VALUES"
+                  + " ('{}'),('{\"val\": \"val1\"}'),"
+                  + " (null)")
+          .execute()
+          .blockLast();
+      sharedConn.createStatement("FLUSH TABLES").execute().blockLast();
+      sharedConn.commitTransaction().block();
+  }
+
+  @AfterAll
+  public static void afterAll2() {
+    //sharedConn.createStatement("DROP TABLE IF EXISTS JsonTable").execute().blockLast();
+  }
+
+  @Test
+  void defaultValue() {
+    defaultValue(sharedConn);
+  }
+
+  @Test
+  void defaultValuePrepare() {
+    defaultValue(sharedConnPrepare);
+  }
+
+  private void defaultValue(MariadbConnection connection) {
+    connection
+        .createStatement("SELECT t1 FROM JsonTable WHERE 1 = ?")
+        .bind(0, 1)
+        .execute()
+        .flatMap(r -> r.map((row, metadata) -> Optional.ofNullable(row.get(0))))
+        .as(StepVerifier::create)
+        .expectNext(
+            Optional.of("{}"),
+            Optional.of("{\"val\": \"val1\"}"),
+            Optional.empty())
+        .verifyComplete();
+
+    connection
+        .createStatement("SELECT t1 FROM JsonTable WHERE 1 = ?")
+        .bind(0, 1)
+        .execute()
+        .flatMap(r -> r.map((row, metadata) -> Optional.ofNullable(row.get(0, Object.class))))
+        .as(StepVerifier::create)
+        .expectNext(
+                Optional.of("{}"),
+                Optional.of("{\"val\": \"val1\"}"),
+                Optional.empty())
+        .verifyComplete();
+  }
+
+  @Test
+  void stringValue() {
+    stringValue(sharedConn);
+  }
+
+  @Test
+  void stringValuePrepare() {
+    stringValue(sharedConnPrepare);
+  }
+
+  private void stringValue(MariadbConnection connection) {
+    connection
+        .createStatement("SELECT t1 FROM JsonTable WHERE 1 = ?")
+        .bind(0, 1)
+        .execute()
+        .flatMap(r -> r.map((row, metadata) -> Optional.ofNullable(row.get(0, String.class))))
+        .as(StepVerifier::create)
+        .expectNext(
+                Optional.of("{}"),
+                Optional.of("{\"val\": \"val1\"}"),
+                Optional.empty())
+        .verifyComplete();
+  }
+
+}
